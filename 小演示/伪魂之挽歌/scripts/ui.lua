96181f1c7d4b014fad610548adc0b2b74c7dcb53
hunzsig <mzyhaohaoren@qq.com>
2022-05-10 21:52:17 +0800

    chore(*): 调整demos

--- 小演示/伪魂之挽歌/scripts/ui.lua
+++ 小演示/伪魂之挽歌/scripts/ui.lua
@@ -1,5 +1,6 @@
-hdzui.wideScreen()
--- hdzui.hideInterface()
+hjapi.DzEnableWideScreen(true)
+--hjapi.DzFrameHideInterface()
+--hjapi.DzFrameEditBlackBorders(0, 0)
 
 UI = function()
     -- 编写属于你的UI
